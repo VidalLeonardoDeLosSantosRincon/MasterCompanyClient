b559ea27545ff1505a9e6118f927daeff0fc48eb
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2023-02-24 12:38:58 -0400

    Adding param route

--- src/Utils/Request/GetEmployeesAll.js
+++ src/Utils/Request/GetEmployeesAll.js
@@ -1,5 +1,5 @@
-export const GetEmployeesAll = async () => {
-    const url = `https://localhost:7124/api/employees`;
+export const GetEmployeesAll = async (route ="") => {
+    const url = `https://localhost:7124/api/employees/${route}`;
     const req = await fetch(url);
     const res = await req.json();
     return res;
